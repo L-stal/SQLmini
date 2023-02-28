8db1b89edc419cb560d86d8f26ac1198f2cfdb6c
Leo <115810719+L-stal@users.noreply.github.com>
2023-02-28 13:05:39 +0100

    gitignore update

--- SqlMini/ProjectModel.cs
+++ SqlMini/ProjectModel.cs
@@ -4,7 +4,11 @@
 
     {
         public int Id { get; set; }
+        public int project_id { get; set; }
         public string Name { get; set; }
+        public int Hours { get; set; }
+
+
 
     }
 }
